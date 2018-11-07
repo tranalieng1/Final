1c9b10142fd1f9e7f987e7ed26aa20926fea37c7
Trần Ngọc An <ngocan15011999@gmail.com>
2018-11-07 20:56:01 +0700

    +add SplashScene

--- Classes/SplashScene.cpp
+++ Classes/SplashScene.cpp
@@ -1,15 +1,15 @@
+﻿#include "SimpleAudioEngine.h"
 #include "SplashScene.h"
-#include "SimpleAudioEngine.h"
-#include "MainMenuScene.h"
-#include "Defnition.h"
-//#define schedule_selector CC_SCHEDULE_SELECTOR
+#include "ui/CocosGUI.h"
 USING_NS_CC;
-using namespace cocos2d;
-//#define schedule_selector
 
-
-
-Scene* SplashScene::createScene()
+SplashScene::SplashScene()
+{
+}
+SplashScene::~SplashScene()
+{
+}
+Scene* SplashScene::createSplScene()
 {
 	auto scene = Scene::create();
 
@@ -20,65 +20,66 @@ Scene* SplashScene::createScene()
 
 }
 
-// Print useful error message instead of segfaulting when files are not there.
-static void problemLoading(const char* filename)
-{
-	printf("Error while loading: %s\n", filename);
-	printf("Depending on how you compiled you might have to add 'Resources/' in front of filenames in HelloWorldScene.cpp\n");
-}
 
-// on "init" you need to initialize your instance
 bool SplashScene::init()
 {
-	//////////////////////////////
-	// 1. super init first
-	if (!Layer::init())
+	////////////////////////////
+
+	if (!Scene::init())
 	{
 		return false;
 	}
 
-	auto visibleSize = Director::getInstance()->getVisibleSize();
-	Vec2 origin = Director::getInstance()->getVisibleOrigin();
-	//SpriteFrameCache::getInstance()->addSpriteFramesWithFile("sprites.plist", "sprites.png");
-	/////////////////////////////
-	this->scheduleOnce(schedule_selector(SplashScene::GotoMainMenuScene), DISPLAY_SPLASH_SCENE);
-	// 2. add a menu item with "X" image, which is clicked to quit the program
-	//    you may modify it.
-	auto backgound = Sprite::create("Background.png");
-	backgound->setPosition(visibleSize / 2);
-	this->addChild(backgound);
-	// add a "close" icon to exit the progress. it's an autorelease object
+	//auto visibleSize = Director::getInstance()->getVisibleSize();
+	//Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
+	createSprSplash();
+	///////////////////////////
+	/*createSprSplash();*/
 	return true;
-
 }
-
-
-
-
-
-void SplashScene::menuCloseCallback(Ref* pSender)
+void SplashScene::createSprSplash()
 {
-	//Close the cocos2d-x game scene and quit the application
-	Director::getInstance()->end();
+	//Back Ground
+	Size winSize = Director::getInstance()->getWinSize();
+	auto backgound = Sprite::create("Background.png");
+	this->addChild(backgound);
+	backgound->setAnchorPoint(Vec2::ANCHOR_MIDDLE);
+	backgound->setPosition(this->getContentSize() * 0.5f);
+	float scaleX = winSize.width*0.8 / (backgound->getContentSize().width);
+	float scaleY = winSize.height*0.8 / (backgound->getContentSize().height);
+	backgound->setScaleX(scaleX);
+	backgound->setScaleY(scaleY);
+	//cái vỏ 
+	auto bgLoading = Sprite::create("LoadingBar.png");
+	this->addChild(bgLoading);
+	bgLoading->setPosition(Vec2(winSize.width * 0.5f, winSize.height * 0.05f));
+	//cái ruột
+	auto loadingBar = ui::LoadingBar::create("LoadingBar1.png");
+	loadingBar->setPosition(Vec2(winSize.width * 0.5f, winSize.height * 0.05f));
+	loadingBar->setDirection(ui::LoadingBar::Direction::LEFT);
+	loadingBar->setPercent(0);
+	this->addChild(loadingBar);
+	this->schedule([=](float delta) {
+		float percen = loadingBar->getPercent();
+		percen++;
+		loadingBar->setPercent(percen);
+		if (percen >= 100.0f)
+		{
+			this->unschedule("updateloadingBar");
+		}
+	}, 0.1f, "updateloadingBar"); //thời gian
+	
+	//this->runAction(Repeat::create(Sequence::createWithTwoActions(countDown,
+	//	DelayTime::create(0.01f)), 40));
 
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
-	exit(0);
-#endif
-
-	/*To navigate back to native iOS screen(if present) without quitting the application  ,do not use Director::getInstance()->end() and exit(0) as given above,instead trigger a custom event created in RootViewController.mm as below*/
-
-	//EventCustom customEndEvent("game_scene_close_event");
-	//_eventDispatcher->dispatchEvent(&customEndEvent);
-
+	//
 
 }
 
-void SplashScene::GotoMainMenuScene(float dt)
-{
-	/*auto scene = MainMenuScene::createScene();
-	Director::getInstance()->replaceScene(TransitionFade::create(TRANSITTION_TIME, scene));*/
 
-	auto scene = MainMenuScene::createScene();
-	Director::getInstance()->replaceScene(TransitionFade::create(TRANSITTION_TIME, scene));
-}
+
+
+
+
+
